1bec077c89702f8a1bab14b8584d4dddc747eeba
leetgit <saujanya99@gmail.com>
2016-08-10 20:13:59 +0545

    Android User Inteface --first commit

---
+++ medical-diagnosis-android/app/src/main/java/com/example/l33t/medical_diagnosis_android/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.l33t.medical_diagnosis_android;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
